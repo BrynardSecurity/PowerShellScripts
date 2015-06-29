646c6e47c475640f59de525b34f0d10a5f3561d1
Chris Smith <christophersmith14@my.unt.edu>
2015-06-29 15:59:36 -0500

    PSscript
    Updated style

--- GetADOU.ps1
+++ GetADOU.ps1
@@ -1,3 +1,8 @@
-Get-ADOrganizationalUnit -Filter * -Properties Name, ObjectClass -SearchBase 'ou=daybreak facilities,ou=daybreak,dc=daybreakventure,dc=loc' | 
-Select Name, Objectclass | Format-Table | 
-Export-Csv C:\Users\ralph.brynard\Documents\GitHub\PowerShellScripts\ADObjects.csv
+Get-ADOrganizationalUnit
+	-Filter * 
+	-Properties Name, ObjectClass 
+		-SearchBase 'ou=daybreak facilities,ou=daybreak,dc=daybreakventure,dc=loc' |
+		Select Name, Objectclass | 
+		Format-Table | 
+		Export-Csv 
+			C:\Users\ralph.brynard\Documents\GitHub\PowerShellScripts\ADObjects.csv
